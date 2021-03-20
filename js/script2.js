9d4f973c8315b83a132b3e47ef7bf845ed80bd55
Gabriela Fernanda <nemesisdarkness897gt@gmail.com>
2021-03-19 23:45:58 -0600

    Pre-Final

--- js/script2.js
+++ js/script2.js
@@ -1,6 +1,6 @@
 $(document).ready(()=>{
     addrow()
-    $("#btnCrear").click(getData)
+    $("#btnCrear").click(buildTicket)
 })
 
 function calctotal(){
@@ -8,12 +8,206 @@ function calctotal(){
     if($(columnas[2]).val().length > 0 && $(columnas[3]).val().length > 0) {
         let cantidad = $(columnas[2]).val();
         let precio = $(columnas[3]).val();
-        $(columnas[4]).attr("value", parseInt(cantidad) * parseInt(precio));
+        $(columnas[4]).attr("value", parseInt(cantidad) * parseFloat(precio));
     }
 }
 
-function getData() {
-    let columnas = $(document).find("input");
+function buildTicket() {
+    var datos = $(document).find("input");
+    var total = 0.0;
+    var iva = 0.0; 
+    var totalAbsoluto = 0.0;
+
+    var rootcard = $('<div></div>');
+        rootcard.addClass('card'); 
+        rootcard.attr('style','margin-top: 5%; margin-left: 20%; margin-right: 20%;');
+    
+    var cardContent = $('<div></div>');
+        cardContent.addClass('card-content');
+        
+        var cardTittle = $('<span></span>');
+            cardTittle.addClass('card-tittle');
+
+            var center =$('<center></center>');
+                var h3 = $('<h3></h3>');
+                    h3.text('** Malon Mart **');
+                    center.append(h3);
+
+                var h4 = $('<h4></h4>');
+                    h4.text(' ************************');
+                    center.append(h4);
+
+                var space = $('<hr/>');
+                    center.append(space);
+
+                var rOw = $('<div></div>');
+                    rOw.addClass('row');
+            
+                    var cOl = $('<div></div>');
+                        cOl.addClass('col s2');
+                        rOw.append(cOl);
+
+                    var cOl = $('<div></div>');
+                        cOl.addClass('col');
+                        cOl.text(' Código');
+                        rOw.append(cOl);
+
+                    var cOl = $('<div></div>');
+                        cOl.addClass('col m2');
+                        cOl.text(' Nombre');
+                        rOw.append(cOl);
+
+                    var cOl = $('<div></div>');
+                        cOl.addClass('col m2');
+                        cOl.text(' Cantidad');
+                        rOw.append(cOl);
+
+                    var cOl = $('<div></div>');
+                        cOl.addClass('col m2');
+                        cOl.text(' Precio');
+                        rOw.append(cOl);
+                
+                    var cOl = $('<div></div>');
+                        cOl.addClass('col m2');
+                        cOl.text(' Subtotal');
+                        rOw.append(cOl);
+
+            center.append(rOw);
+    
+        cardTittle.append(center);    
+
+        
+    let filas = datos.length/5; 
+
+    for(i=0;i<filas;i++){
+                var center =$('<center></center>');    
+                    var rOw = $('<div></div>');
+                        rOw.addClass('row');
+     
+                    var cOl = $('<div></div>');
+                        cOl.addClass('col s2');
+                        rOw.append(cOl);
+
+                    var cOl = $('<div></div>');
+                        cOl.addClass('col');
+                        cOl.text($(datos[i*5]).val());
+                        rOw.append(cOl);
+
+                    var cOl = $('<div></div>');
+                        cOl.addClass('col m2');
+                        cOl.attr("style", "text-align: center;")
+                        cOl.text($(datos[i*5+1]).val());
+                        rOw.append(cOl);
+
+                    var cOl = $('<div></div>');
+                        cOl.addClass('col m2');
+                        cOl.text($(datos[i*5+2]).val());
+                        rOw.append(cOl);
+
+                    var cOl = $('<div></div>');
+                        cOl.addClass('col m2');
+                        cOl.text($(datos[i*5+3]).val());
+                        rOw.append(cOl);
+         
+                    var cOl = $('<div></div>');
+                        cOl.addClass('col m2');
+                        cOl.text($(datos[i*5+4]).val());
+                        total = parseFloat($(datos[i*5+4]).val()) + total;
+                        rOw.append(cOl);
+
+            center.append(rOw);
+
+        cardTittle.append(center);    
+
+    }
+            var center =$('<center></center>');
+                var rOw = $('<div></div>');
+                    rOw.addClass('row');
+            
+                    var cOl = $('<div></div>');
+                        cOl.addClass('col s8');
+                        rOw.append(cOl);
+                    var cOl = $('<div></div>');
+                        cOl.addClass('col m2');
+                        cOl.text('Subtotal');
+                        rOw.append(cOl);
+                    var cOl = $('<div></div>');
+                        cOl.addClass('col center');
+                        cOl.text(total);
+                        rOw.append(cOl);
+
+            center.append(rOw);
+
+            var rOw = $('<div></div>');
+                rOw.addClass('row');
+            
+                    var cOl = $('<div></div>');
+                        cOl.addClass('col s8');
+                        rOw.append(cOl);
+                    var cOl = $('<div></div>');
+                        cOl.addClass('col m2');
+                        cOl.text('Iva');
+                        rOw.append(cOl);      
+                    var cOl = $('<div></div>');
+                        cOl.addClass('col center');
+                        cOl.text('16%');
+                        iva = total * 0.16;
+                        rOw.append(cOl);   
+            center.append(rOw);
+
+                    var rOw = $('<div></div>');
+                        rOw.addClass('row');
+                            var cOl = $('<div></div>');
+                                cOl.addClass('col s8');
+                        rOw.append(cOl);
+                            var cOl = $('<div></div>');
+                                cOl.addClass('col m2');
+                                cOl.text('Total');
+                        rOw.append(cOl);
+                            var cOl = $('<div></div>');
+                                cOl.addClass('col center');
+                                totalAbsoluto = total + iva;
+                                cOl.text(totalAbsoluto);        
+                        rOw.append(cOl);
+            center.append(rOw);
+
+                    var rOw = $('<div></div>');
+                        rOw.addClass('row');
+            
+                    var cOl = $('<div></div>');
+                        cOl.addClass('row');
+
+                        let idx = 'MLMRCX_' + datos.length + $(datos[0]).val() + datos.length * $(datos[4]).val()
+
+                        cOl.text('ID:' + idx)
+                        rOw.append(cOl);
+            center.append(rOw);
+
+                    var space = $('<hr/>');
+            center.append(space);
+
+                    var h6 = $('<h6></h6>');
+                        h6.text(' Call center: +52 777 341 2411 ');
+                        center.append(h6);
+                    var h7 = $('<h6></h6>');
+                        h7.text(' #Hailgrasa ');
+                        center.append(h7);
+                    var h5 = $('<h6></h6>');
+                        h5.text(' #HelloHello el diseño es mi pasión Chueck ');
+                        center.append(h5);
+                    var h4 = $('<h4></h4>');
+                        h4.text(' ************************');
+                        center.append(h4);
+
+                    var h8 = $('<h8></h8>');
+                        h8.text('System: @Hana897TRX (Gabriela) / Karimo64 (Karim) / Chucho (Jesus Guerrero)');
+                        center.append(h8);
+
+                cardTittle.append(center); 
+            cardContent.append(cardTittle);
+        rootcard.append(cardContent); 
+    $('#receiptContent').append(rootcard);
+
 }   
 
 function addrow() {
@@ -90,3 +284,4 @@ function delet(){
     $(this).parents()[1].remove()
 
 }
+
